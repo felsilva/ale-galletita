fd06b59bee2e6c277eb1932544e13ace7ff35859
Felipe Silva <feisipos@gmail.com>
2025-01-21 20:40:36 -0300

    Refactor build process and update asset paths for PWA
    - Updated the build script in package.json to use Windows commands for copying assets to the public directory.
    - Changed asset paths in the service worker to reflect the new directory structure, moving images and icons to a 'src' subdirectory.
    - Removed the icons.json file and associated icon assets, streamlining the icon management for the application.

--- public/sw.js
+++ public/sw.js
@@ -5,46 +5,15 @@ const urlsToCache = [
   '/styles.css',
   '/script.js',
   '/manifest.json',
-  '/src/1.gif',
-  '/icons/72/icon-72x72.png',
-  '/icons/96/icon-96x96.png',
-  '/icons/128/icon-128x128.png',
-  '/icons/144/icon-144x144.png',
-  '/icons/152/icon-152x152.png',
-  '/icons/192/icon-192x192.png',
-  '/icons/384/icon-384x384.png',
-  '/icons/512/icon-512x512.png'
+  '/src/images/1.gif',
+  '/src/icons/72/icon-72x72.png',
+  '/src/icons/96/icon-96x96.png',
+  '/src/icons/128/icon-128x128.png',
+  '/src/icons/144/icon-144x144.png',
+  '/src/icons/152/icon-152x152.png',
+  '/src/icons/192/icon-192x192.png',
+  '/src/icons/384/icon-384x384.png',
+  '/src/icons/512/icon-512x512.png'
 ];
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      })
-  );
-});
-
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.filter(cacheName => {
-          return cacheName !== CACHE_NAME;
-        }).map(cacheName => {
-          return caches.delete(cacheName);
-        })
-      );
-    })
-  );
-}); 
+// ... resto del código ...
